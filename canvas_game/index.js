5d100c764b42dd177664725c24a549eea90b5936
amdzu <amdzu@yahoo.com>
2021-07-21 22:00:19 +0300

    vers

--- canvas_game/index.js
+++ canvas_game/index.js
@@ -1,10 +1,15 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+
+var boxWidth = 75;
+var boxHeight = 40;
+
+var x = canvas.width;
+var y = canvas.height/2-boxHeight/2;
+
+var dx = 0.5;
+var dy = -0.5;
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
@@ -20,31 +25,57 @@ var brickOffsetLeft = 30;
 var score = 0;
 var lives = 3;
 
-var boxWidth = 75;
-var boxHeight = 40;
+var textWidth=80;
+var totalTextwidth=0;
+var textSpacing=15;
+
 var i=0;
+
 var word = ['стол','стул','слон','сон'];
+var wordX=[];
+var lWord = word.length;
+
+var txtColor='red';
 
 
 
-var bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
-  bricks[c] = [];
-  for(var r=0; r<brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0, status: 1 };
-  }
-}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+
 document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("click", clickHandler, false);
+
+function clickHandler(e) {
+    var oY = canvas.offsetTop;
+    var oX = canvas.offsetLeft;
+    var relativeX = e.clientX - oX;
+    var relativeY = e.clientY - oY;
+
+    
+
+
+
+
+
+    if(relativeX > x-25 && relativeX < x+textWidth+25 && relativeY>y-10 && relativeY<y+45) {
+        txtColor='blue';
+    } else {txtColor='red'};
+}
+
 
 function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if(relativeX > 0 && relativeX < canvas.width) {
-        paddleX = relativeX - paddleWidth/2;
-    }
+    var oY = canvas.offsetTop;
+    var oX = canvas.offsetLeft;
+    var relativeX = e.clientX - oX;
+    var relativeY = e.clientY - oY;
+   if(relativeX > x-25 && relativeX < x+textWidth+25 && relativeY>y-10 && relativeY<y+45) {
+     canvas.style.cursor = "pointer";
+    } else {canvas.style.cursor="default"};
+
 }
+
+
 
 function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
@@ -67,98 +98,37 @@ function keyUpHandler(e) {
 
 
 
-
-function drawPaddle() {
-  ctx.beginPath();
-  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
+function drawWord(text,h,v) {
+  textWidth = ctx.measureText(text).width;
+  totalTextwidth=totalTextwidth+textWidth+textSpacing;
+  ctx.font = "26px Arial";
+  ctx.fillStyle = txtColor;
+  ctx.fillText(text, h, v+13);
 }
-
-
-
-
-
-function drawScore() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "red";
-  ctx.fillText("Score: "+score+1+1, paddleX, canvas.height-paddleHeight,);
-
-
-  ctx.fillText("Score: "+score+5, paddleX, canvas.height-paddleHeight+10,);
-}		
-
-
-
-
-function drawWord(i) {
-
-  var j=i;
-  var text=word[j];
-  ctx.beginPath();
-  ctx.rect(x, y, boxWidth, boxHeight);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "red";
-  ctx.fillText(text, x, y);
-
-
-}
-
-
 
 
 
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+totalTextwidth=0;
 
+for (var i=0;i<lWord;i++) {
 
+var text=word[i];
+wordX[i]=x+totalTextwidth;
+console.log(wordX[i]);
+drawWord(text,x+totalTextwidth,y);
 
-drawWord(i);
-i++;
-
-
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if(y + dy < ballRadius) {
-    dy = -dy;
-  }
-  else if(y + dy > canvas.height-ballRadius) {
-    if(x > paddleX && x < paddleX + paddleWidth) {
-      dy = -dy;
-    }
-    else {
-      lives--;
-      if(!lives) {
-          alert("GAME OVER");
-          document.location.reload();
-          clearInterval(interval); // Needed for Chrome to end game
-      }
-      else {
-          x = canvas.width/2;
-          y = canvas.height-30;
-          dx = 2;
-          dy = -2;
-          paddleX = (canvas.width-paddleWidth)/2;
-      }
-    }
-  }
-
-  if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    paddleX += 7;
-  }
-  else if(leftPressed && paddleX > 0) {
-    paddleX -= 7;
-  }
-
-  x += dx;
- // y += dy;
 }
 
-var interval = setInterval(draw, 15);
+
+
+
+if (x<-totalTextwidth) {x = canvas.width; txtColor='red';}
+  x -= dx;
+}
+
+
+
+var interval = setInterval(draw, 50);
